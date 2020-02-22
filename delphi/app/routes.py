e2e4635bf8ffd3b2a553499d001b408b81dc454a
Savon Phillips <31143072+savonphillips@users.noreply.github.com>
2020-02-22 13:56:51 -0600

    #4 - Setup basic file upload

--- delphi/app/routes.py
+++ delphi/app/routes.py
@@ -1,6 +1,15 @@
 from app import app
+from flask import request
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return 'Hello, World!'
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    for f in request.files.getlist('file'):
+        f.save(f'uploads/{f.filename}')
+    return f'File(s) successfully uploaded'
