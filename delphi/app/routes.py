eaed4e0be319adc65cbb0347816adc2cd2d95ccb
Savon Phillips <31143072+savonphillips@users.noreply.github.com>
2020-02-26 07:50:50 -0600

    #4 - decode buffer into image

--- delphi/app/routes.py
+++ delphi/app/routes.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import request
+import cv2
+import numpy
 
 
 @app.route('/')
@@ -10,6 +12,12 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    for f in request.files.getlist('file'):
-        f.save(f'uploads/{f.filename}')
-    return f'File(s) successfully uploaded'
+    if request.headers['content-length'] != '0' and request.headers['content-type'] in ['image/jpeg', 'image/png']:
+        buf = numpy.frombuffer(request.data, numpy.uint8)
+        current_image = cv2.imdecode(buf, cv2.IMREAD_COLOR)
+        if current_image is None:
+            return 'Failed to receive image', 500
+        else:
+            return 'Image received', 200
+    else:
+        return 'Request must contain an image', 415
