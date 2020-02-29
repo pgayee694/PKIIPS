25bca7aafd28ba1b241efe7d54d7874e3240c1fc
Savon Phillips <31143072+savonphillips@users.noreply.github.com>
2020-02-29 17:21:56 -0600

    #14 - update endpoint

--- delphi/app/routes.py
+++ delphi/app/routes.py
@@ -1,7 +1,5 @@
 from app import app
 from flask import request
-import cv2
-import numpy
 
 
 @app.route('/')
@@ -10,14 +8,14 @@ def index():
     return 'Hello, World!'
 
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if request.headers['content-length'] != '0' and request.headers['content-type'] in ['image/jpeg', 'image/png']:
-        buf = numpy.frombuffer(request.data, numpy.uint8)
-        current_image = cv2.imdecode(buf, cv2.IMREAD_COLOR)
-        if current_image is None:
-            return 'Failed to receive image', 500
-        else:
-            return 'Image received', 200
-    else:
-        return 'Request must contain an image', 415
+@app.route('/update-sensor-data', methods=['POST'])
+def updateSensorData():
+    data = request.get_json()
+    sensorId = data.get('id')
+    count = data.get('count')
+
+    if sensorId is not None and isinstance(sensorId, int) and count is not None and isinstance(count, int):
+        # todo: store sensor data
+        return 'Sensor data saved successfully', 200
+
+    return 'Failed to save sensor data'
