c398201735da0312c24d5755f11151989ae3d050
Savon Phillips <31143072+savonphillips@users.noreply.github.com>
2020-02-29 18:27:18 -0600

    #14 - update cont.

--- delphi/app/routes.py
+++ delphi/app/routes.py
@@ -2,20 +2,16 @@ from app import app
 from flask import request
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return 'Hello, World!'
-
-
 @app.route('/update-sensor-data', methods=['POST'])
 def updateSensorData():
-    data = request.get_json()
-    sensorId = data.get('id')
-    count = data.get('count')
+    data = request.get_json(silent=True)
 
-    if sensorId is not None and isinstance(sensorId, int) and count is not None and isinstance(count, int):
-        # todo: store sensor data
-        return 'Sensor data saved successfully', 200
+    if data is not None:
+        sensorId = data.get('id')
+        count = data.get('count')
 
-    return 'Failed to save sensor data'
+        if sensorId is not None and isinstance(sensorId, int) and count is not None and isinstance(count, int):
+            # todo: store sensor data
+            return '', 204
+
+    return 'Invalid Request Syntax', 400
