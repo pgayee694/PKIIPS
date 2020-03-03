f76885142886ca0c8e04e9ce51bffbe0ffe0d525
Suhvon <31143072+Suhvon@users.noreply.github.com>
2020-03-02 20:36:29 -0600

    #14 - review changes final

--- delphi/app/routes.py
+++ delphi/app/routes.py
@@ -9,9 +9,8 @@ def updateSensorData():
         sensorId = int(data.get('id'))
         count = int(data.get('count'))
 
-        if sensorId is not None and count is not None:
-            # todo: store sensor data
-            return '', 204
+        # todo: store sensor data
+        return '', 204
     except:
         pass
 
