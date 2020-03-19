4121d201ef996419b3e003417a7238da3ec1eff6
Savon Phillips <31143072+savonphillips@users.noreply.github.com>
2020-03-18 19:35:15 -0500

    Update peoplecount plugin to accept video

--- seers/tests/test_peoplecount.py
+++ seers/tests/test_peoplecount.py
@@ -3,44 +3,57 @@ import seer_config
 import seer_plugin
 import plugins.people_count
 
+
 class TestPeopleCountPlugin(unittest.TestCase):
-	MODEL_PATH = 'MobileNetSSD_deploy.caffemodel'
-	PROTO_PATH = 'MobileNetSSD_deploy.prototxt.txt'
+    MODEL_PATH = 'MobileNetSSD_deploy.caffemodel'
+    PROTO_PATH = 'MobileNetSSD_deploy.prototxt.txt'
+    STREAM_TYPE = 'file'
+    FILE_STREAM_PATH = 'tests/test_vid.mp4'
 
-	def test_collect(self):
-		seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.MODEL_INI]		= TestPeopleCountPlugin.MODEL_PATH
-		seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.PROTOTXT_INI]	= TestPeopleCountPlugin.PROTO_PATH
+    def setUp(self):
+        seer_config.configuration[plugins.people_count.PeopleCount.INI][
+            plugins.people_count.PeopleCount.STREAM_TYPE_INI] = TestPeopleCountPlugin.STREAM_TYPE
+        seer_config.configuration[plugins.people_count.PeopleCount.INI][
+            plugins.people_count.PeopleCount.FILE_STREAM_PATH_INI] = TestPeopleCountPlugin.FILE_STREAM_PATH
 
-		def testing_collect(deliv, data):
-			count_data = data.get(plugins.people_count.PeopleCount.COUNT_KEY)
-			self.assertIsInstance(count_data, int)
-			deliv.stop()
+    def test_collect(self):
+        seer_config.configuration[plugins.people_count.PeopleCount.INI][
+            plugins.people_count.PeopleCount.MODEL_INI] = TestPeopleCountPlugin.MODEL_PATH
+        seer_config.configuration[plugins.people_count.PeopleCount.INI][
+            plugins.people_count.PeopleCount.PROTOTXT_INI] = TestPeopleCountPlugin.PROTO_PATH
 
-		delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
-			testing_collect, timeout=None)
+        def testing_collect(deliv, data):
+            count_data = data.get(plugins.people_count.PeopleCount.COUNT_KEY)
+            self.assertIsInstance(count_data, int)
+            deliv.stop()
 
-		delivery.start()
+        delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
+                                              testing_collect, timeout=None)
 
-	def test_init_missing_model(self):
-		seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.MODEL_INI] = ''
+        delivery.start()
 
-		def testing_init(deliv, data):
-			self.assertFalse(plugins.people_count.PeopleCount.COUNT_KEY in data)
-			deliv.stop()
+    def test_init_missing_model(self):
+        seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.MODEL_INI] = ''
 
-		delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
-			testing_init, timeout=5000)
+        def testing_init(deliv, data):
+            self.assertFalse(
+                plugins.people_count.PeopleCount.COUNT_KEY in data)
+            deliv.stop()
 
-		delivery.start()
+        delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
+                                              testing_init, timeout=5000)
 
-	def test_init_missing_prototxt(self):
-		seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.PROTOTXT_INI] = ''
+        delivery.start()
 
-		def testing_init(deliv, data):
-			self.assertFalse(plugins.people_count.PeopleCount.COUNT_KEY in data)
-			deliv.stop()
+    def test_init_missing_prototxt(self):
+        seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.PROTOTXT_INI] = ''
 
-		delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
-			testing_init, timeout=5000)
+        def testing_init(deliv, data):
+            self.assertFalse(
+                plugins.people_count.PeopleCount.COUNT_KEY in data)
+            deliv.stop()
 
-		delivery.start()
+        delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
+                                              testing_init, timeout=5000)
+
+        delivery.start()
