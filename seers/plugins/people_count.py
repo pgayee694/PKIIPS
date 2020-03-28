25b98a9fe70f988e3f7f4fb0ef6686d75359865b
Savon Phillips <31143072+savonphillips@users.noreply.github.com>
2020-03-27 19:08:17 -0500

    Revert "Update peoplecount plugin to accept video"
    This reverts commit 4121d201ef996419b3e003417a7238da3ec1eff6.

--- seers/plugins/people_count.py
+++ seers/plugins/people_count.py
@@ -6,137 +6,120 @@ import imutils.video
 import numpy
 import os.path
 
-
 class PeopleCount(seer_plugin.DataCollectorPlugin):
-    """
-    Data collector plugin that gathers video feed
-    and uses models to determine the count of people
-    in the video stream.
-    """
+	"""
+	Data collector plugin that gathers video feed
+	and uses models to determine the count of people
+	in the video stream.
+	"""
 
-    BACKGROUND_CLASS = "background"
-    AEROPLANE_CLASS = "aeroplane"
-    BICYCLE_CLASS = "bicycle"
-    BIRD_CLASS = "bird"
-    BOAT_CLASS = "boat"
-    BOTTLE_CLASS = "bottle"
-    BUS_CLASS = "bus"
-    CAR_CLASS = "car"
-    CAT_CLASS = "cat"
-    CHAIR_CLASS = "chair"
-    COW_CLASS = "cow"
-    DININGTABLE_CLASS = "diningtable"
-    DOG_CLASS = "dog"
-    HORSE_CLASS = "horse"
-    MOTORBIKE_CLASS = "motorbike"
-    PERSON_CLASS = "person"
-    POTTEDPLANT_CLASS = "pottedplant"
-    SHEEP_CLASS = "sheep"
-    SOFA_CLASS = "sofa"
-    TRAIN_CLASS = "train"
-    TVMONITOR_CLASS = "tvmonitor"
+	BACKGROUND_CLASS 	= "background"
+	AEROPLANE_CLASS		= "aeroplane"
+	BICYCLE_CLASS		= "bicycle"
+	BIRD_CLASS 			= "bird"
+	BOAT_CLASS 			= "boat"
+	BOTTLE_CLASS 		= "bottle"
+	BUS_CLASS 			= "bus"
+	CAR_CLASS 			= "car"
+	CAT_CLASS 			= "cat"
+	CHAIR_CLASS 		= "chair"
+	COW_CLASS 			= "cow"
+	DININGTABLE_CLASS 	= "diningtable"
+	DOG_CLASS 			= "dog"
+	HORSE_CLASS 		= "horse"
+	MOTORBIKE_CLASS 	= "motorbike"
+	PERSON_CLASS 		= "person"
+	POTTEDPLANT_CLASS 	= "pottedplant"
+	SHEEP_CLASS 		= "sheep"
+	SOFA_CLASS 			= "sofa"
+	TRAIN_CLASS 		= "train"
+	TVMONITOR_CLASS 	= "tvmonitor"
 
-    CLASSES = \
-        [
-            BACKGROUND_CLASS,
-            AEROPLANE_CLASS,
-            BICYCLE_CLASS,
-            BIRD_CLASS,
-            BOAT_CLASS,
-            BOTTLE_CLASS,
-            BUS_CLASS,
-            CAR_CLASS,
-            CAT_CLASS,
-            CHAIR_CLASS,
-            COW_CLASS,
-            DININGTABLE_CLASS,
-            DOG_CLASS,
-            HORSE_CLASS,
-            MOTORBIKE_CLASS,
-            PERSON_CLASS,
-            POTTEDPLANT_CLASS,
-            SHEEP_CLASS,
-            SOFA_CLASS,
-            TRAIN_CLASS,
-            TVMONITOR_CLASS,
-        ]
+	CLASSES = \
+	[
+		BACKGROUND_CLASS 	,
+		AEROPLANE_CLASS		,
+		BICYCLE_CLASS		,
+		BIRD_CLASS 			,
+		BOAT_CLASS 			,
+		BOTTLE_CLASS 		,
+		BUS_CLASS 			,
+		CAR_CLASS 			,
+		CAT_CLASS 			,
+		CHAIR_CLASS 		,
+		COW_CLASS 			,
+		DININGTABLE_CLASS 	,
+		DOG_CLASS 			,
+		HORSE_CLASS 		,
+		MOTORBIKE_CLASS 	,
+		PERSON_CLASS 		,
+		POTTEDPLANT_CLASS 	,
+		SHEEP_CLASS 		,
+		SOFA_CLASS 			,
+		TRAIN_CLASS 		,
+		TVMONITOR_CLASS 	,
+	]
 
-    INI = 'people-count'
-    CONFIDENCE_INI = 'confidence'
-    MODEL_INI = 'model-path'
-    PROTOTXT_INI = 'prototxt-path'
+	INI 			= 'people-count'
+	CONFIDENCE_INI	= 'confidence'
+	MODEL_INI		= 'model-path'
+	PROTOTXT_INI	= 'prototxt-path'
 
-    DEFAULT_CONFIDENCE = 0.2
-    COUNT_KEY = 'count'
+	DEFAULT_CONFIDENCE	= 0.2
+	COUNT_KEY			= 'count'
 
-    STREAM_TYPE_INI = 'stream-type'
-    DEFAULT_STREAM_TYPE = 'camera'
-    FILE_STREAM_PATH_INI = 'file-stream-path'
+	def init(self):
+		"""
+		Gets configuration for confidence and model paths.
+		Sets up the network and video stream.
+		"""
+		self.confidence = seer_config.configuration[PeopleCount.INI].getfloat(PeopleCount.CONFIDENCE_INI, fallback=PeopleCount.DEFAULT_CONFIDENCE)
+		self.model		= seer_config.configuration[PeopleCount.INI].get(PeopleCount.MODEL_INI)
+		self.prototxt	= seer_config.configuration[PeopleCount.INI].get(PeopleCount.PROTOTXT_INI)
 
-    def init(self):
-        """
-        Gets configuration for confidence and model paths.
-        Sets up the network and video stream.
-        """
-        self.confidence = seer_config.configuration[PeopleCount.INI].getfloat(
-            PeopleCount.CONFIDENCE_INI, fallback=PeopleCount.DEFAULT_CONFIDENCE)
-        self.model = seer_config.configuration[PeopleCount.INI].get(
-            PeopleCount.MODEL_INI)
-        self.prototxt = seer_config.configuration[PeopleCount.INI].get(
-            PeopleCount.PROTOTXT_INI)
-        self.streamType = seer_config.configuration[PeopleCount.INI].get(
-            PeopleCount.STREAM_TYPE_INI, fallback=PeopleCount.DEFAULT_STREAM_TYPE)
+		if not os.path.isfile(self.model):
+			raise IOError(self.model)
+		if not os.path.isfile(self.prototxt):
+			raise IOError(self.prototxt)
 
-        if not os.path.isfile(self.model):
-            raise IOError(self.model)
-        if not os.path.isfile(self.prototxt):
-            raise IOError(self.prototxt)
+		self.net		= cv2.dnn.readNetFromCaffe(self.prototxt, self.model)
+		self.video		= imutils.video.VideoStream().start()
 
-        self.net = cv2.dnn.readNetFromCaffe(self.prototxt, self.model)
+	def shutdown(self):
+		"""
+		Stops the video stream.
+		"""
+		self.video.stop()
 
-        if self.streamType == PeopleCount.DEFAULT_STREAM_TYPE:
-            self.video = imutils.video.VideoStream().start()
-        else:
-            self.video = imutils.video.FileVideoStream(
-                seer_config.configuration[PeopleCount.INI].get(
-                    PeopleCount.FILE_STREAM_PATH_INI)
-            ).start()
+	def collect(self):
+		"""
+		Collects a frame from the video stream, runs it through
+		the network, and count the number of model detections
+		in the frame.
 
-    def shutdown(self):
-        """
-        Stops the video stream.
-        """
-        self.video.stop()
+		Returns:
+		(dictionary): A dictionary with one key: PeopleCount.COUNT_KEY,
+					  with its value being the count of people in the current
+					  frame of the video feed.
+		"""
+		frame = self.video.read()
+		frame = imutils.resize(frame, width=400)
 
-    def collect(self):
-        """
-        Collects a frame from the video stream, runs it through
-        the network, and count the number of model detections
-        in the frame.
+		height, width	= frame.shape[:2]
+		blob			= cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+			0.007843, (300, 300), 127.5)
 
-        Returns:
-        (dictionary): A dictionary with one key: PeopleCount.COUNT_KEY,
-                                  with its value being the count of people in the current
-                                  frame of the video feed.
-        """
-        frame = self.video.read()
-        frame = imutils.resize(frame, width=400)
+		self.net.setInput(blob)
+		detections 		= self.net.forward()
+		detection_count = 0
 
-        height, width = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-                                     0.007843, (300, 300), 127.5)
+		for i in numpy.arange(0, detections.shape[2]):
+			confidence = detections[0, 0, i, 2]
 
-        self.net.setInput(blob)
-        detections = self.net.forward()
-        detection_count = 0
+			if confidence > self.confidence:
+				idx = int(detections[0, 0, i, 1])
+				if PeopleCount.CLASSES[idx] != PeopleCount.PERSON_CLASS:
+					continue
+				detection_count += 1
 
-        for i in numpy.arange(0, detections.shape[2]):
-            confidence = detections[0, 0, i, 2]
-
-            if confidence > self.confidence:
-                idx = int(detections[0, 0, i, 1])
-                if PeopleCount.CLASSES[idx] != PeopleCount.PERSON_CLASS:
-                    continue
-                detection_count += 1
-
-        return {PeopleCount.COUNT_KEY: detection_count}
+		return {PeopleCount.COUNT_KEY: detection_count}
--- seers/tests/test_peoplecount.py
+++ seers/tests/test_peoplecount.py
@@ -3,57 +3,44 @@ import seer_config
 import seer_plugin
 import plugins.people_count
 
-
 class TestPeopleCountPlugin(unittest.TestCase):
-    MODEL_PATH = 'MobileNetSSD_deploy.caffemodel'
-    PROTO_PATH = 'MobileNetSSD_deploy.prototxt.txt'
-    STREAM_TYPE = 'file'
-    FILE_STREAM_PATH = 'tests/test_vid.mp4'
+	MODEL_PATH = 'MobileNetSSD_deploy.caffemodel'
+	PROTO_PATH = 'MobileNetSSD_deploy.prototxt.txt'
 
-    def setUp(self):
-        seer_config.configuration[plugins.people_count.PeopleCount.INI][
-            plugins.people_count.PeopleCount.STREAM_TYPE_INI] = TestPeopleCountPlugin.STREAM_TYPE
-        seer_config.configuration[plugins.people_count.PeopleCount.INI][
-            plugins.people_count.PeopleCount.FILE_STREAM_PATH_INI] = TestPeopleCountPlugin.FILE_STREAM_PATH
+	def test_collect(self):
+		seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.MODEL_INI]		= TestPeopleCountPlugin.MODEL_PATH
+		seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.PROTOTXT_INI]	= TestPeopleCountPlugin.PROTO_PATH
 
-    def test_collect(self):
-        seer_config.configuration[plugins.people_count.PeopleCount.INI][
-            plugins.people_count.PeopleCount.MODEL_INI] = TestPeopleCountPlugin.MODEL_PATH
-        seer_config.configuration[plugins.people_count.PeopleCount.INI][
-            plugins.people_count.PeopleCount.PROTOTXT_INI] = TestPeopleCountPlugin.PROTO_PATH
+		def testing_collect(deliv, data):
+			count_data = data.get(plugins.people_count.PeopleCount.COUNT_KEY)
+			self.assertIsInstance(count_data, int)
+			deliv.stop()
 
-        def testing_collect(deliv, data):
-            count_data = data.get(plugins.people_count.PeopleCount.COUNT_KEY)
-            self.assertIsInstance(count_data, int)
-            deliv.stop()
+		delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
+			testing_collect, timeout=None)
 
-        delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
-                                              testing_collect, timeout=None)
+		delivery.start()
 
-        delivery.start()
+	def test_init_missing_model(self):
+		seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.MODEL_INI] = ''
 
-    def test_init_missing_model(self):
-        seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.MODEL_INI] = ''
+		def testing_init(deliv, data):
+			self.assertFalse(plugins.people_count.PeopleCount.COUNT_KEY in data)
+			deliv.stop()
 
-        def testing_init(deliv, data):
-            self.assertFalse(
-                plugins.people_count.PeopleCount.COUNT_KEY in data)
-            deliv.stop()
+		delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
+			testing_init, timeout=5000)
 
-        delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
-                                              testing_init, timeout=5000)
+		delivery.start()
 
-        delivery.start()
+	def test_init_missing_prototxt(self):
+		seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.PROTOTXT_INI] = ''
 
-    def test_init_missing_prototxt(self):
-        seer_config.configuration[plugins.people_count.PeopleCount.INI][plugins.people_count.PeopleCount.PROTOTXT_INI] = ''
+		def testing_init(deliv, data):
+			self.assertFalse(plugins.people_count.PeopleCount.COUNT_KEY in data)
+			deliv.stop()
 
-        def testing_init(deliv, data):
-            self.assertFalse(
-                plugins.people_count.PeopleCount.COUNT_KEY in data)
-            deliv.stop()
+		delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
+			testing_init, timeout=5000)
 
-        delivery = seer_plugin.PluginDelivery([plugins.people_count.PeopleCount],
-                                              testing_init, timeout=5000)
-
-        delivery.start()
+		delivery.start()
