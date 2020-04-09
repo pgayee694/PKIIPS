6f5b4eacca35ac9af1cefaa1252ecca6c6123fd7
pgayee694 <pgayee694@gmail.com>
2020-04-09 01:31:38 -0500

    stereo calibration

--- seers/calibration/calibrate.py
+++ seers/calibration/calibrate.py
@@ -19,8 +19,8 @@ if args.path:
         print('Invalid or empty directory')
         sys.exit()
     else:
-        imagesl = glob.glob('{}l/*.jpg'.format(args.path))
-        imagesr = glob.glob('{}r/*.jpg'.format(args.path))
+        imagesl = glob.glob('{}*l.jpg'.format(args.path))
+        imagesr = glob.glob('{}*r.jpg'.format(args.path))
 
 
 # termination criteria
@@ -46,12 +46,15 @@ else:
 
 num_pics = 0
 
-video = None
-if not imagesl or not imagesr:
+shapel = None
+shaper = None
+
+if imagesl == None or imagesr == None:
     videol = cv2.VideoCapture(0)
     videor = cv2.VideoCapture(1)
 
-    videol.set(cv2.CAP_PROP_FRAME_HEIGHT, )
+    #videol.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+    videor.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 
     while num_pics < num:
         ret, framel = videol.read()
@@ -59,6 +62,12 @@ if not imagesl or not imagesr:
         grayl = cv2.cvtColor(framel, cv2.COLOR_BGR2GRAY)
         grayr = cv2.cvtColor(framer, cv2.COLOR_BGR2GRAY)
 
+        #cv2.imshow('framel', framel)
+        #cv2.imshow('framer', framer)
+
+        #cv2.waitKey(1000)
+
+        print('looking for corners')
         foundl, cornersl = cv2.findChessboardCorners(grayl, (9,6), None)
         foundr, cornersr = cv2.findChessboardCorners(grayr, (9,6), None)
         if foundl == foundr == True:
@@ -74,42 +83,39 @@ if not imagesl or not imagesr:
             start = time.time()
             now = start
 
-            while now - start_time < 5:
+            while now - start < 5:
                 # avoid the video advancing when we don't want it to
 
                 framel = videol.read()
                 framer = videor.read()
-                cv2.imshow('left', framel)
-                cv2.imshow('right', framer)
+                #cv2.imshow('left', framel)
+                #cv2.imshow('right', framer)
 
-                cv2.waitKey(1)
+                #cv2.waitKey(1)
                 now = time.time()
 
+        #cv2.waitKey(1)
+    
     videol.release()
     videor.release()
     cv2.destroyAllWindows()
 else:
     # We already have an image directory
 
-    for imagel in imagesl:
-        img = cv2.imread(imagel)
-        grayl = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    for imagel, imager in zip(imagesl, imagesr):
+        imgl = cv2.imread(imagel)
+        grayl = cv2.cvtColor(imgl, cv2.COLOR_BGR2GRAY)
 
-        found, corners = cv2.findChessboardCorners(grayl, (9,6), None)
+        imgr = cv2.imread(imager)
+        grayr = cv2.cvtColor(imgr, cv2.COLOR_BGR2GRAY)
 
-        if found == True:
+        foundl, cornersl = cv2.findChessboardCorners(grayl, (9,6), None)
+        foundr, cornersr = cv2.findChessboardCorners(grayr, (9,6), None)
+
+        if foundl == foundr == True:
             objpoints.append(objp)
-            imgpointsl.append(corners)
-    
-    for imager in imagesr:
-        img = cv2.imread(imager)
-        grayr = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        found, corners = cv2.findChessboardCorners(grayr, (9,6), None)
-
-        if found == True:
-            objpoints.append(objp)
-            imgpointsr.append(corners)
+            imgpointsl.append(cornersl)
+            imgpointsr.append(cornersr)
 
 # Actual calibration
 retl, mtxl, distl, rvecsl, tvecsl = cv2.calibrateCamera(objpoints, imgpointsl, grayl.shape[::-1], None, None)
@@ -119,7 +125,7 @@ imgl = cv2.imread('./images/chessboard-5l.jpg')
 h, w = imgl.shape[:2]
 newcameramtxl, roil = cv2.getOptimalNewCameraMatrix(mtxl, distl, (w,h), 1, (w,h))
 
-undistorted = cv2.undistort(imgl, mtxl, distl, None, newcameramtx)
+undistorted = cv2.undistort(imgl, mtxl, distl, None, newcameramtxl)
 x, y, w, h = roil
 undistorted = undistorted[y:y+h, x:x+w]
 cv2.imwrite('./generated/resultl.jpg', undistorted)
@@ -128,7 +134,7 @@ imgr = cv2.imread('./images/chessboard-5r.jpg')
 h, w = imgr.shape[:2]
 newcameramtxr, roir = cv2.getOptimalNewCameraMatrix(mtxr, distr, (w,h), 1, (w,h))
 
-undistorted = cv2.undistort(imgr, mtxr, distr, None, newcameramtx)
+undistorted = cv2.undistort(imgr, mtxr, distr, None, newcameramtxr)
 x, y, w, h = roir
 undistorted = undistorted[y:y+h, x:x+w]
 cv2.imwrite('./generated/resultr.jpg', undistorted)
@@ -145,9 +151,28 @@ for i in range(len(objpoints)):
     imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecsr[i], tvecsr[i], mtxr, distr)
     error = cv2.norm(imgpointsr[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
     mean_error += error
-print( "total error(right): {}".format(mean_error/len(objpoints)) )
+print( "total error(right): {}".format(mean_error/len(objpoints)) ) 
 
 # Stereo stuff
-ret, mtxl, distl, mtxr, distr, R, T, E, F = cv2.stereoCalibrate(objpoints, imgpointsl, imgpointsr, mtxl, distl, mtxr, distr, grayl.shape[:2], None, None, None, None, flags=cv2.CALIB_FIX_INTRINSIC)
+error, mtxl, distl, mtxr, distr, R, T, E, F = cv2.stereoCalibrate(objpoints, imgpointsl, imgpointsr, mtxl, distl, mtxr, distr, grayl.shape[:2], None, None, None, None, flags=cv2.CALIB_FIX_INTRINSIC + cv2.CALIB_RATIONAL_MODEL + cv2.CALIB_FIX_PRINCIPAL_POINT)
 
-print('stereo calibraiton error: {}'.format(ret))
+rectifl, rectifr, projl, projr, disparityToDepthMap, roil, roir = cv2.stereoRectify(mtxl, distl, mtxr, distr, grayl.shape[:2], R, T, None, None, None, None, None, cv2.CALIB_ZERO_DISPARITY)
+
+mapXl, mapYl = cv2.initUndistortRectifyMap(mtxl, distl, rectifl, projl, grayl.shape[:2], cv2.CV_32FC1)
+
+mapXr, mapYr = cv2.initUndistortRectifyMap(mtxr, distr, rectifr, projr, grayr.shape[:2], cv2.CV_32FC1)
+
+framel = cv2.imread('./images/chessboard-10l.jpg')
+framer = cv2.imread('./images/chessboard-10r.jpg')
+
+undistorted_rectifiedl = cv2.remap(framel, mapXl, mapYl, cv2.INTER_LINEAR)
+undistorted_rectifiedr = cv2.remap(framer, mapXr, mapYr, cv2.INTER_LINEAR)
+
+cv2.imshow('rectifiedl', undistorted_rectifiedl)
+cv2.imshow('rectifiedr', undistorted_rectifiedr)
+
+cv2.waitKey(0)
+
+np.savez_compressed('./generated/calibration', imageSize=grayl.shape[:2], leftMapX=mapXl, leftMapY=mapYl, leftROI=roil, rightMapX=mapXr, rightMapY=mapYr, rightROI=roir)
+
+print('stereo calibraiton error: {}'.format(error))
