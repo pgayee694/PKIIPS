658f17c694945c37881a37f9daf5f1d9e80eedd3
Savon Phillips <31143072+savonphillips@users.noreply.github.com>
2020-04-23 14:40:12 -0500

    updated client + config setup

--- oracle/Assets/Scripts/DelphiClient.cs
+++ oracle/Assets/Scripts/DelphiClient.cs
@@ -1,94 +1,122 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Linq;
 using SimpleJSON;
 
 /// <summary>
-/// Client used to make calls to and from Delphi.
+/// Client used to communicate with Delphi.
 /// </summary>
 public class DelphiClient : MonoBehaviour
 {
-  // todo: move to config
-  private readonly string BASE_URL = "localhost:5000";
+    /// <summary>
+    /// Base URL of all of Delphi's endpoints.
+    /// </summary>
+    private static readonly string BASE_URL = Config.cfg["delphi client"]["base-url"].StringValue;
 
-  /// <summary>
-  /// Updates all of the fields associated with a room.
-  /// </summary>
-  public void UpdateRoom(Room room)
-  {
-    StartCoroutine(CallGetCounts(new int[] { room.Id }, (response) => room.PeopleCount = response[room.Id]));
-    // todo: update room status
-    // StartCoroutine(CallGetStatuses(new int[] { room.Id }, (response) => room.Status = response[room.Id]));
-  }
+    /// <summary>
+    /// Single instance of the client.
+    /// </summary>
+    private static DelphiClient _instance;
 
-  /// <summary>
-  /// Helper method used to generate a list of parameters.
-  /// </summary>
-  string GenerateParams(string key, int[] values)
-  {
-    string output = "?";
-    for (int i = 0; i < values.Length; i++)
+    /// <summary>
+    /// Publicly accessible client instance.
+    /// </summary>
+    public static DelphiClient Instance
     {
-      if (i != 0) output += "&";
-      output += key + "=" + values[i];
-    }
-    return output;
-  }
-
-  /// <summary>
-  /// Gets the status of all of the rooms given in the list of ids.
-  /// </summary>
-  IEnumerator CallGetStatuses(int[] roomIds, System.Action<JSONNode> callback)
-  {
-    UnityWebRequest www = UnityWebRequest.Get(BASE_URL + "/get-statuses" + GenerateParams("room_id", roomIds));
-    yield return www.SendWebRequest();
-
-    if (www.isNetworkError || www.isHttpError)
-    {
-      Debug.LogError(www.error);
-      yield break;
+        get
+        {
+            if (_instance == null)
+                _instance = new GameObject("DelphiClient").AddComponent<DelphiClient>();
+            return _instance;
+        }
     }
 
-    JSONNode response = JSON.Parse(www.downloadHandler.text);
 
-    callback(response);
-  }
-
-  /// <summary>
-  /// Gets the people count of all of the rooms given in the list of ids.
-  /// </summary>
-  IEnumerator CallGetCounts(int[] roomIds, System.Action<JSONNode> callback)
-  {
-    UnityWebRequest www = UnityWebRequest.Get(BASE_URL + "/get-counts" + GenerateParams("room_id", roomIds));
-    yield return www.SendWebRequest();
-
-    if (www.isNetworkError || www.isHttpError)
+    /// <summary>
+    /// Prevents the client from being destroyed on scene change.
+    /// </summary>
+    void Awake()
     {
-      Debug.LogError(www.error);
-      yield break;
+        if (_instance != null) Destroy(this);
+        DontDestroyOnLoad(this);
     }
 
-    JSONNode response = JSON.Parse(www.downloadHandler.text);
 
-    callback(response);
-  }
-
-  /// <summary>
-  /// Updates the status of the room associated with the given id.
-  /// </summary>
-  IEnumerator CallUpdateStatus(int id, string status)
-  {
-    JSONObject requestData = new JSONObject();
-    requestData.Add("enable", status);
-
-    UnityWebRequest www = UnityWebRequest.Put(BASE_URL + "/enable/" + id, requestData.ToString());
-    www.SetRequestHeader("Content-Type", "application/json");
-    yield return www.SendWebRequest();
-
-    if (www.isNetworkError || www.isHttpError)
+    /// <summary>
+    /// Updates all of the fields for each of the rooms supplied.
+    /// </summary>
+    public void UpdateRooms(Room[] rooms)
     {
-      Debug.LogError(www.error);
-      yield break;
+        int[] roomIds = rooms.Select(room => room.Id).ToArray();
+        StartCoroutine(CallGetCounts(roomIds, (response) => rooms.ToList().ForEach(room => room.PeopleCount = response[room.Id])));
+        // todo: update room status
     }
-  }
+
+
+    /// <summary>
+    /// Helper method used to generate a list of parameters.
+    /// </summary>
+    private string GenerateParams(string key, int[] values)
+    {
+        return "?" + string.Join("&", values.Select(value => key + "=" + value).ToArray());
+    }
+
+
+    /// <summary>
+    /// Gets the status of all of the rooms given in the list of ids.
+    /// </summary>
+    IEnumerator CallGetStatuses(int[] roomIds, System.Action<JSONNode> callback)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(BASE_URL + "/get-statuses" + GenerateParams("room_id", roomIds));
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+            yield break;
+        }
+
+        JSONNode response = JSON.Parse(www.downloadHandler.text);
+
+        callback(response);
+    }
+
+    /// <summary>
+    /// Gets the people count of all of the rooms given in the list of ids.
+    /// </summary>
+    IEnumerator CallGetCounts(int[] roomIds, System.Action<JSONNode> callback)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(BASE_URL + "/get-counts" + GenerateParams("room_id", roomIds));
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+            yield break;
+        }
+
+        JSONNode response = JSON.Parse(www.downloadHandler.text);
+
+        callback(response);
+    }
+
+    /// <summary>
+    /// Updates the status of the room associated with the given id.
+    /// </summary>
+    IEnumerator CallUpdateStatus(int id, string status)
+    {
+        JSONObject requestData = new JSONObject();
+        requestData.Add("enable", status);
+
+        UnityWebRequest www = UnityWebRequest.Put(BASE_URL + "/enable/" + id, requestData.ToString());
+        www.SetRequestHeader("Content-Type", "application/json");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+            yield break;
+        }
+    }
 }
--- oracle/Assets/Scripts/Floor.cs
+++ oracle/Assets/Scripts/Floor.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// Script to be placed onto a game object that contains
@@ -9,56 +7,57 @@ using UnityEngine;
 /// </summary>
 public class Floor : MonoBehaviour
 {
-  /// <summary>
-  /// Sets the delay at which the UpdateRooms is called in seconds.
-  /// </summary>
-  private static float DELAY_UPDATE_TIME = 0.0f;
+    /// <summary>
+    /// Sets the delay at which the UpdateRooms is called in seconds.
+    /// </summary>
+    private static float DELAY_UPDATE_TIME = 0.0f;
 
-  /// <summary>
-  /// Sets the interval at which the UpdateRooms is called in seconds.
-  /// </summary>
-  private static float UPDATE_INTERVAL_TIME = 0.5f;
+    /// <summary>
+    /// Sets the interval at which the UpdateRooms is called in seconds.
+    /// </summary>
+    private static float UPDATE_INTERVAL_TIME = 0.5f;
 
-  /// <summary>
-  /// Client used to update the rooms of the floor.
-  /// </summary>
-  public DelphiClient client;
+    /// <summary>
+    /// The collection of rooms that belong to the floor.
+    /// </summary>
+    private Room[] rooms;
 
-  /// <summary>
-  /// Called when this game object is created.
-  /// </summary>
-  void Start()
-  {
-    client = gameObject.AddComponent<DelphiClient>();
-    InvokeRepeating("UpdateRooms", DELAY_UPDATE_TIME, UPDATE_INTERVAL_TIME);
-  }
 
-  /// <summary>
-  /// Continiously called to update the rooms of the floor.
-  /// Updates according to the value of the <code>UPDATE_INTERVAL_TIME</code> property.
-  /// <see cref="UPDATE_INTERVAL_TIME"/>
-  /// </summary>
-  void UpdateRooms()
-  {
-    // todo: generate list of room ids to update in a single call
-    foreach (var room in GetComponentsInChildren<Room>())
+    /// <summary>
+    /// Called when this game object is created.
+    /// </summary>
+    void Start()
     {
-      client.UpdateRoom(room);
-    }
-  }
-
-  /// <summary>
-  /// Called when this game object is destroyed.
-  /// Will destroy all children <code>Node</code>s.
-  /// <see cref="Node"/>
-  /// </summary>
-  void OnDestroy()
-  {
-    foreach (var node in GetComponentsInChildren<GraphComponent>())
-    {
-      Destroy(node);
+        rooms = GetComponentsInChildren<Room>();
+        InvokeRepeating("UpdateRooms", DELAY_UPDATE_TIME, UPDATE_INTERVAL_TIME);
     }
 
-    Destroy(gameObject);
-  }
+    /// <summary>
+    /// Called when this game object is destroyed.
+    /// Will destroy all children <code>Node</code>s.
+    /// <see cref="Node"/>
+    /// </summary>
+    void OnDestroy()
+    {
+        foreach (var node in GetComponentsInChildren<GraphComponent>())
+        {
+            Destroy(node);
+        }
+
+        Destroy(gameObject);
+    }
+
+
+    /// <summary>
+    /// Continiously called to update the rooms of the floor.
+    /// Updates according to the value of the <code>UPDATE_INTERVAL_TIME</code> property.
+    /// <see cref="UPDATE_INTERVAL_TIME"/>
+    /// </summary>
+    void UpdateRooms()
+    {
+        if (rooms.Length > 0)
+        {
+            DelphiClient.Instance.UpdateRooms(rooms);
+        }
+    }
 }
